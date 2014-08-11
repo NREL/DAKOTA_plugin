bc8c6324ad4b516386b74af62d7aede41095619e
Scott Townsend <scott.e.townsend@nasa.gov>
2014-08-11 15:48:13 -0400

    pyDAKOTA updated for DAKOTA 6.0+ (svn trunk > 2707).

--- pyDAKOTA/setup.py
+++ pyDAKOTA/setup.py
@@ -1,17 +1,17 @@
 # Copyright 2013 National Renewable Energy Laboratory (NREL)
-# 
+#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
-# 
+#
 #        http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-# 
+#
 # ++==++==++==++==++==++==++==++==++==++==
 """
 Build pyDAKOTA Python 'egg' for cygwin, darwin, linux, or win32 platforms.
@@ -37,7 +37,6 @@ This has been tested on a 'vanilla' (no DAKOTA pre-installed) Windows machine.
 
 import glob
 import os.path
-import shutil
 import subprocess
 import sys
 
@@ -50,6 +49,7 @@ from setuptools.extension import Extension
 import numpy
 numpy_include = os.path.dirname(numpy.__file__)+'/core/include'
 
+
 # Execute DAKOTA to get version.
 try:
     proc = subprocess.Popen(['dakota', '-v'], universal_newlines=True,
@@ -60,15 +60,17 @@ except Exception, exc:
     sys.exit(1)
 
 fields = stdout.split()
-if len(fields) >= 3 and fields[0] == 'DAKOTA' and fields[1] == 'version':
+if len(fields) >= 3 and \
+   fields[0].upper() == 'DAKOTA' and fields[1] == 'version':
     dakota_version = fields[2]
-    wrapper_version = '1'
-    egg_dir = 'pyDAKOTA-%s_%s-py%s-%s.egg' % (dakota_version, wrapper_version,
-                                              sys.version[0:3], get_build_platform())
 else:
     print "Can't parse version from DAKOTA output %r" % stdout
     print "    stderr output:", stderr
     sys.exit(1)
+
+wrapper_version = '1'
+egg_dir = 'pyDAKOTA-%s_%s-py%s-%s.egg' % (dakota_version, wrapper_version,
+                                          sys.version[0:3], get_build_platform())
 
 # Assuming standard prefix-based install.
 dakota_install = os.path.dirname(
@@ -82,6 +84,18 @@ if not os.path.exists(dakota_bin) or \
    not os.path.exists(dakota_lib):
     print "Can't find", dakota_bin, 'or', dakota_include, 'or', dakota_lib, ', bummer'
     sys.exit(1)
+
+# Read make macros from `install_dir`/include/Makefile.export.Dakota.
+dakota_macros = {}
+with open(os.path.join(dakota_install, 'include',
+                       'Makefile.export.Dakota'), 'rU') as inp:
+    for line in inp:
+        line = line.strip()
+        if not line or line.startswith('#'):
+            continue
+        name, _, value = line.partition('=')
+        dakota_macros[name.strip()] = value.strip().split()
+
 
 # Set to a list of any special compiler flags required.
 CXX_FLAGS = []
@@ -114,17 +128,12 @@ EXTRA_LIBS = []
 EGG_LIBS = []
 
 # Set True to include MPI support.
-NEED_MPI = False
-
-# Set HAVE_ABORT_RETURNS True if DAKOTA has been updated to include the
-# abort_returns flag.  This allows model exceptions to be passed back to the
-# driver rather than causing the simulation to exit.
-HAVE_ABORT_RETURNS = False
+NEED_MPI = '-DDAKOTA_HAVE_MPI' in dakota_macros['Dakota_DEFINES']
 
 
-if sys.platform == 'cygwin':
+if sys.platform == 'cygwin':  # Only tested with DAKOTA 5.3.
     BOOST_LIBFMT = '%s-mt'
-    EXTRA_LIBS = ['DGraphics', 'gfortran',
+    EXTRA_LIBS = ['gfortran',
                   'SM', 'ICE', 'Xext', 'Xm', 'Xt', 'X11', 'Xpm', 'Xmu']
     EGG_LIBS = ['C:/cygwin/bin/cygboost_python-mt-1_50.dll',
                 'C:/cygwin/bin/libpython2.7.dll']
@@ -135,23 +144,22 @@ if sys.platform == 'cygwin':
 
 elif sys.platform == 'win32':
     CXX_FLAGS = ['/EHsc']
-    BOOST_INCDIR = r'C:\Users\setowns1\Downloads\boost_1_50_0'
-    BOOST_LIBDIR = r'C:\Users\setowns1\Downloads\boost_1_50_0\stage\lib'
-    BOOST_LIBFMT = 'lib%s-vc90-mt-1_50'
-    BOOST_PYFMT = '%s-vc90-mt-1_50'  # Only built boost_python shared.
+    BOOST_INCDIR = r'C:\Users\setowns1\Downloads\boost_1_54_0'
+    BOOST_LIBDIR = r'C:\Users\setowns1\Downloads\boost_1_54_0\stage\lib'
+    # The normal DAKOTA build links Boost libraries statically. But attempting a
+    # static link of boost_python here doesn't work. So with the BOOST_LIBFMT
+    # and BOOST_PYFMT definitions below we get statically-linked Boost, except
+    # for boost_python which refers to a .dll that we must then deliver.
+    # We probably could just use a dynamic Boost for everything, but then we
+    # have to deliver more files for no apparent gain.
+    BOOST_LIBFMT = 'lib%s-vc90-mt-1_54'  # Default Boost static build name.
+    BOOST_PYFMT = '%s-vc90-mt-1_54'      # Name from Boost dynamic build.
     LAPACK_LIBDIR = r'C:\Users\setowns1\Downloads\lapack-3.4.1\install\lib'
     FORTRAN_LIBDIR = r'C:\Program Files\Intel\Composer XE 2013\compiler\lib\ia32'
-    EXTRA_LIBS = ['Dbghelp']
-    EGG_LIBS = [
-        r'C:\Users\setowns1\Downloads\boost_1_50_0\stage\lib\boost_python-vc90-mt-1_50.dll',
-        r'C:\Program Files\Intel\Composer XE 2013\redist\ia32\compiler\libifcoremd.dll',
-        r'C:\Program Files\Intel\Composer XE 2013\redist\ia32\compiler\libmmd.dll',
-        r'C:\Program Files\Intel\Composer XE 2013\redist\ia32\compiler\svml_dispmd.dll',
-        r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\Microsoft.VC90.CRT.manifest',
-        r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\msvcm90.dll',
-        r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\msvcp90.dll',
-        r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\msvcr90.dll']
-    HAVE_ABORT_RETURNS = True
+    EXTRA_LIBS = ['Dbghelp', 'winmm']
+    EGG_LIBS = glob.glob(os.path.join(dakota_bin, '*.dll'))
+    EGG_LIBS.extend([os.path.join(dakota_bin, 'Microsoft.VC90.CRT.manifest'),
+                     os.path.join(BOOST_LIBDIR, 'boost_python-vc90-mt-1_54.dll')])
 
 elif sys.platform == 'darwin':
     # This builds an egg, but it doesn't load:
@@ -163,39 +171,31 @@ elif sys.platform == 'darwin':
     # (DAKOTA libraries are only i386)
     BOOST_INCDIR = '/Users/setowns1/include'
     BOOST_LIBDIR = '/Users/setowns1/boost_1_50_0/stage/lib'
-    EXTRA_LIBS = ['DGraphics', 'gfortran.3', 'Xm.3']
+    EXTRA_LIBS = ['gfortran.3', 'Xm.3']
     EGG_LIBS = glob.glob(os.path.join(dakota_lib, '*.dylib'))
     EGG_LIBS.extend(glob.glob(os.path.join(dakota_bin, '*.dylib')))
-    NEED_MPI = True
 
 else:
     # This LD_FLAGS stuff avoids having to set LD_LIBRARY_PATH to access
     # the other (not pyDAKOTA.so) shared libraries that are part of the egg.
+    BOOST_INCDIR = '/home/setowns1/include'
+    BOOST_LIBDIR = '/home/setowns1/boost_1_49_0/stage/lib'
     LD_FLAGS = ['-Wl,-z origin',
                 '-Wl,-rpath=${ORIGIN}:${ORIGIN}/../'+egg_dir]
-    EXTRA_LIBS = ['DGraphics', 'gfortran',
+    EXTRA_LIBS = ['gfortran',
                   'SM', 'ICE', 'Xext', 'Xm', 'Xt', 'X11', 'Xpm', 'Xmu']
     EGG_LIBS = glob.glob(os.path.join(dakota_lib, '*.so'))
     EGG_LIBS.extend(glob.glob(os.path.join(dakota_bin, '*.so')))
-    NEED_MPI = True
-    HAVE_ABORT_RETURNS = True
 
 
-sources = [
-    'dakface.cpp', 'dakota_python_binding.cpp', 'python_interface.cpp',
-]
+sources = ['dakface.cpp', 'dakota_python_binding.cpp']
 
 include_dirs = [dakota_include, numpy_include]
 if BOOST_INCDIR:
     include_dirs.append(BOOST_INCDIR)
 
-# HAVE_AMPL *must* match install (at least for v5.3) for structure size match.
-define_macros = [
-    ('DAKOTA_PYTHON_NUMPY', None),
-    ('HAVE_AMPL', None),
-]
-if HAVE_ABORT_RETURNS:
-    define_macros.append(('HAVE_ABORT_RETURNS', None))
+# Drop '-D' from Dakota_DEFINES.
+define_macros = [(name[2:], None) for name in dakota_macros['Dakota_DEFINES']]
 
 # Some DAKOTA distributions (i.e. cygwin) put libraries in 'bin'.
 library_dirs = [dakota_lib, dakota_bin]
@@ -206,24 +206,17 @@ if LAPACK_LIBDIR:
 if FORTRAN_LIBDIR:
     library_dirs.append(FORTRAN_LIBDIR)
 
-# Based on TPL LIBS displayed near end of cmake output.
-# 'jega' repeated for non-rescanning linkers.
-dakota_libs = [
-    'dakota_src', 'dakota_src_fortran',
-    'nidr', 'teuchos', 'pecos', 'pecos_src', 'lhs', 'mods', 'mod', 'dfftpack',
-    'sparsegrid', 'surfpack', 'surfpack_fortran', 'colin', 'interfaces',
-    'scolib', '3po', 'pebbl', 'utilib', 'tinyxml', 'conmin', 'dace',
-    'analyzer', 'random', 'sampling', 'bose', 'fsudace', 'hopspack',
-    'jega_fe', 'jega', 'moga', 'soga', 'jega', 'eutils', 'utilities', 'ncsuopt',
-    'cport', 'optpp', 'psuade', 'amplsolver']
+# Drop '-l' from Dakota_LIBRARIES if necessary.
+dakota_libs = dakota_macros['Dakota_LIBRARIES']
+if dakota_libs[0].startswith('-l'):
+    dakota_libs = [name[2:] for name in dakota_libs]
 
-# Based on EXTRA TPL LIBS displayed near end of cmake output.
+# From Makefile.export.Dakota Dakota_TPL_LIBRARIES.
 external_libs = [
-    'boost_regex', 'boost_filesystem', 'boost_system', 'boost_signals',
-    'boost_python', 'lapack', 'blas']
+    'boost_regex', 'boost_filesystem', 'boost_serialization', 'boost_system',
+    'boost_signals', 'boost_python', 'lapack', 'blas']
 
 if NEED_MPI:
-    define_macros.append(('DAKOTA_HAVE_MPI', None))
     external_libs.append('boost_mpi')
     os.environ['CC'] = 'mpicxx'  # Force compiler command.
 
--- pyDAKOTA/test_dakota.py
+++ pyDAKOTA/test_dakota.py
@@ -1,34 +1,38 @@
 # Copyright 2013 National Renewable Energy Laboratory (NREL)
-# 
+#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
-# 
+#
 #        http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-# 
+#
 # ++==++==++==++==++==++==++==++==++==++==
 """
-Trivial pyDAKOTA test which runs a study on a 'model' which is the
+Trivial pyDAKOTA test which runs an optimization on a 'model' which is the
 Rosenbrock function.
 """
 
 from numpy import array
+from traceback import print_exc
+import unittest
 
-from dakota import DakotaInput, run_dakota
+from dakota import DakotaBase
 
 
-class TestDriver(object):
+class TestDriver(DakotaBase):
 
     def __init__(self):
-        self.input = DakotaInput()
+        super(TestDriver, self).__init__()
+        self.force_exception = False
+
         self.input.method = [
-            "optpp_newton",
+            "conmin_frcg", #"optpp_newton",
             "  max_iterations = 50",
             "  convergence_tolerance = 1e-4",
         ]
@@ -45,50 +49,41 @@ class TestDriver(object):
             "analytic_hessians",
         ]
 
-    def run_dakota(self):
-        """
-        Call DAKOTA, providing self as `data`.
-        DAKOTA will then call our :meth:`dakota_callback` during the run.
-        """
-        infile = 'test_driver.in'
-        self.input.write_input(infile)
-        run_dakota(infile, data=self)
-
     def dakota_callback(self, **kwargs):
         """
         Return responses from parameters.  `kwargs` contains:
 
-        ========== ==============================================
-        Key        Definition
-        ========== ==============================================
-        functions  number of functions (responses, constraints)
-        ---------- ----------------------------------------------
-        variables  total number of variables
-        ---------- ----------------------------------------------
-        cv         list/array of continuous variable values
-        ---------- ----------------------------------------------
-        div        list/array of discrete integer variable values
-        ---------- ----------------------------------------------
-        drv        list/array of discrete real variable values
-        ---------- ----------------------------------------------
-        av         single list/array of all variable values
-        ---------- ----------------------------------------------
-        cv_labels  continuous variable labels
-        ---------- ----------------------------------------------
-        div_labels discrete integer variable labels
-        ---------- ----------------------------------------------
-        drv_labels discrete real variable labels
-        ---------- ----------------------------------------------
-        av_labels  all variable labels
-        ---------- ----------------------------------------------
-        asv        active set vector (bit1=f, bit2=df, bit3=d^2f)
-        ---------- ----------------------------------------------
-        dvv        derivative variables vector
-        ---------- ----------------------------------------------
-        currEvalId current evaluation ID number
-        ---------- ----------------------------------------------
-        user_data  this object
-        ========== ==============================================
+        =================== ==============================================
+        Key                 Definition
+        =================== ==============================================
+        functions           number of functions (responses, constraints)
+        ------------------- ----------------------------------------------
+        variables           total number of variables
+        ------------------- ----------------------------------------------
+        cv                  list/array of continuous variable values
+        ------------------- ----------------------------------------------
+        div                 list/array of discrete integer variable values
+        ------------------- ----------------------------------------------
+        drv                 list/array of discrete real variable values
+        ------------------- ----------------------------------------------
+        av                  single list/array of all variable values
+        ------------------- ----------------------------------------------
+        cv_labels           continuous variable labels
+        ------------------- ----------------------------------------------
+        div_labels          discrete integer variable labels
+        ------------------- ----------------------------------------------
+        drv_labels          discrete real variable labels
+        ------------------- ----------------------------------------------
+        av_labels           all variable labels
+        ------------------- ----------------------------------------------
+        asv                 active set vector (bit1=f, bit2=df, bit3=d^2f)
+        ------------------- ----------------------------------------------
+        dvv                 derivative variables vector
+        ------------------- ----------------------------------------------
+        currEvalId          current evaluation ID number
+        ------------------- ----------------------------------------------
+        analysis_components str(id(self))
+        =================== ==============================================
         
         """
         print 'dakota_callback:'
@@ -114,9 +109,12 @@ class TestDriver(object):
 
             if asv[0] & 4:
                 fx = x[1]-3*x[0]*x[0]
-                h = array([ [ [-400*fx + 2, -400*x[0]],
-                              [-400*x[0],    200     ] ] ] )
+                h = array([[[-400*fx + 2, -400*x[0]],
+                            [-400*x[0],    200     ]]])
                 retval['fnHessians'] = h
+
+            if self.force_exception:
+                raise RuntimeError('Forced exception')
 
         except Exception, exc:
             print '    caught', exc
@@ -126,7 +124,50 @@ class TestDriver(object):
         return retval
 
 
+class TestCase(unittest.TestCase):
+
+    def test_dakota(self):
+        # To exercise recovery from exceptions, all tests are run within this.
+        driver = TestDriver()
+
+        print '\n### Check normal run.'
+        driver.run_dakota()
+
+        print '\n### Check Python exception handling.'
+        driver.force_exception = True
+        driver.input.method[2] = "  convergence_tolerance = 1e-5"  # Force eval.
+        try:
+            driver.run_dakota()
+        except RuntimeError as exc:
+            self.assertEqual(str(exc), 'Forced exception')
+        else:
+            self.fail('Expected a forced Python exception')
+
+        print '\n### Check normal run after exception.'
+        driver.force_exception = False
+        driver.run_dakota()
+
+        print '\n### Check Python exception handling again.'
+        driver.force_exception = True
+        driver.input.variables[1] = "  cdv_initial_point  -1.2  2.0"  # Force eval.
+        try:
+            driver.run_dakota()
+        except RuntimeError as exc:
+            self.assertEqual(str(exc), 'Forced exception')
+        else:
+            self.fail('Expected a forced Python exception')
+
+        print '\n### Check bad input handling.'
+        driver.input.method[0] = 'no-such-method'
+        try:
+            driver.run_dakota()
+        except RuntimeError as exc:
+            msg = 'Dakota aborted: Unknown error'  # Linux adds '-2'
+            self.assertEqual(str(exc)[:len(msg)], msg)
+        else:
+            self.fail('Expected an exception for bad input')
+
+
 if __name__ == '__main__':
-    driver = TestDriver()
-    driver.run_dakota()
+    unittest.main()
 
